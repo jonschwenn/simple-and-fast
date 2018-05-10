74052e7f1acd0d8a378f218baccf10818d6674dc
jschwenn <jschwenn@digitalocean.com>
2018-05-10 15:31:55 -0500

    typo

--- deploy.sh
+++ deploy.sh
@@ -61,7 +61,7 @@ create_loadbalancer(){
   curl -X POST https://api.digitalocean.com/v2/load_balancers \
   -H "Content-Type: application/json" \
   -H "Authorization: Bearer $DO_TOKEN"  \
-  -d '{"name": "web-balanver", "region": "nyc1", "forwarding_rules":[{"entry_protocol":"http","entry_port":80,"target_protocol":"http","target_port":80,"certificate_id":"","tls_passthrough":false}, {"entry_protocol": "https","entry_port": 443,"target_protocol": "https","target_port": 443,"tls_passthrough": true}], "health_check":{"protocol":"http","port":80,"path":"/","check_interval_seconds":10,"response_timeout_seconds":5,"healthy_threshold":5,"unhealthy_threshold":3}, "sticky_sessions":{"type":"none"}, "tag": "web"}'
+  -d '{"name": "web-balancer", "region": "nyc1", "forwarding_rules":[{"entry_protocol":"http","entry_port":80,"target_protocol":"http","target_port":80,"certificate_id":"","tls_passthrough":false}, {"entry_protocol": "https","entry_port": 443,"target_protocol": "https","target_port": 443,"tls_passthrough": true}], "health_check":{"protocol":"http","port":80,"path":"/","check_interval_seconds":10,"response_timeout_seconds":5,"healthy_threshold":5,"unhealthy_threshold":3}, "sticky_sessions":{"type":"none"}, "tag": "web"}'
 }
 
 main() {
